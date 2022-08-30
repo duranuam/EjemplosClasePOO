5a3597fa75a12c6795ffd4f92c9c8207c0e413f2
Laboratorio C205 <labc205@uam.net.ni>
2022-08-30 09:47:49 -0600

    Avances de la clas del 30 de agosto

---
+++ Sesion3008/src/sesion3008/Sesion3008.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sesion3008;
+//Data
+//Access
+
+import modelos.Persona;
+import modelos.Sexo;
+
+//Object
+
+/**
+ *
+ * @author labc205
+ */
+public class Sesion3008 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
